b428f3d2bfd65e872916d2fdcb3d8708209306a9
Marshall Eddy <MarshallEddy@users.noreply.github.com>
2017-10-16 13:53:18 -0600

    Add files via upload

---
+++ CS221/RecursiveSortingAndSearching/SearchAndSort.java
@@ -0,0 +1,113 @@
+import java.util.*;
+
+/**
+ * Class for searching and sorting DoubleLinkedLists 
+ * using either natural order or a Comparator.
+ *
+ * @author spanter, mvail
+ */
+public class SearchAndSort
+{
+	/**
+	 * Sorts a list that implements the DoubleLinkedListADT using the
+	 * natural ordering of list elements.
+	 * DO NOT MODIFY THIS METHOD SIGNATURE
+	 * 
+	 * @param <T>
+	 *            The data type in the list must extend Comparable
+	 * @param list
+	 *            The list that will be sorted
+	 * @see DoubleLinkedListADT 
+	 */
+	public static <T extends Comparable<T>> void sort(DoubleLinkedListADT<T> list) {
+
+	}
+
+	/**
+	 * Sorts a DoubleLinkedListADT with the provided Comparator.
+	 * DO NOT MODIFY THIS METHOD SIGNATURE
+	 * 
+	 * @param <T>
+	 *            The type of list to sort
+	 * @param list
+	 *            The list to sort
+	 * @param c
+	 *            The Comparator to use
+	 * @see DoubleLinkedListADT
+	 */
+	public static <T> void sort(DoubleLinkedListADT<T> list, Comparator<T> c) {
+
+	}
+
+	/**
+	 * Finds the smallest element in a list according to the natural ordering of 
+	 * list elements. Does not alter the order of list elements.
+	 * DO NOT MODIFY THIS METHOD SIGNATURE
+	 * 
+	 * @param <T>
+	 *            The type of object we are comparing
+	 * @param list
+	 *            The list we are passed
+	 * @return The smallest element or null if list is empty
+	 * @see DoubleLinkedListADT
+	 */
+	public static <T extends Comparable<T>> T findSmallest(DoubleLinkedListADT<T> list) {
+
+		return null; //TODO replace
+	}
+
+	/**
+	 * Finds the smallest element in a list with a Custom comparator. Does not
+	 * alter the order of list elements.
+	 * DO NOT MODIFY THIS METHOD SIGNATURE
+	 * 
+	 * @param <T>
+	 *            The type of object we are comparing
+	 * @param list
+	 *            The list we are passed
+	 * @param c
+	 *            The comparator to use
+	 * @return The smallest element or null if list is empty
+	 * @see DoubleLinkedListADT
+	 */
+	public static <T> T findSmallest(DoubleLinkedListADT<T> list, Comparator<T> c) {
+
+		return null; //TODO replace
+	}
+
+	/**
+	 * Finds the largest element in a list according to the natural ordering of
+	 * list elements. Does not alter the order of list elements.
+	 * DO NOT MODIFY THIS METHOD SIGNATURE
+	 * 
+	 * @param <T>
+	 *            The type of object we are comparing
+	 * @param list
+	 *            The list we are passed
+	 * @return The largest element or null if list is empty
+	 * @see DoubleLinkedListADT
+	 */
+	public static <T extends Comparable<T>> T findLargest(DoubleLinkedListADT<T> list) {
+
+		return null; //TODO replace
+	}
+
+	/**
+	 * Finds the largest element in a list with a Custom comparator. Does not
+	 * alter the order of list elements.
+	 * DO NOT MODIFY THIS METHOD SIGNATURE
+	 * 
+	 * @param <T>
+	 *            The type of object we are comparing
+	 * @param list
+	 *            The list we are passed
+	 * @param c
+	 *            The comparator to use
+	 * @return The largest element or null if list is empty
+	 * @see DoubleLinkedListADT
+	 */
+	public static <T> T findLargest(DoubleLinkedListADT<T> list, Comparator<T> c) {
+
+		return null; //TODO replace
+	}
+}
